6aea874af2da245738b3a03ed64e444ed0b88791
DrDalek <sonny.ra@hotmail.fr>
2020-06-28 18:23:11 +0200

    Procédure

---
+++ Controllers/ProcedureController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcClients.Controllers
+{
+	public class ProcedureController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult RJ45()
+		{
+			return View();
+		}
+
+		public IActionResult CPL()
+		{
+			return View();
+		}
+
+		public IActionResult Wifi()
+		{
+			return View();
+		}
+
+	}
+}
